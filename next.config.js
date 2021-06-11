eaeb626ac36f618f11fc00fc5a8344e894e042e3
Savo <savo.kos.sk@gmail.com>
2021-06-11 08:13:08 +0200

    firebasestorage.googleapis.com image domain added

--- next.config.js
+++ next.config.js
@@ -5,10 +5,11 @@ module.exports = {
       'http://localhost:3000',
       'https://savokos.com',
       'https://excellence.savokos.com',
+      'excellence.savokos.com',
       'https://firebasestorage.googleapis.com',
-      'http://firebasestorage.googleapis.com',
       'https://firebasestorage.googleapis.com/v0/b/excellence-holdings.appspot.com/o/images',
       'https://excellence.savokos.com/nextimg',
+      'firebasestorage.googleapis.com',
     ],
   },
 };
