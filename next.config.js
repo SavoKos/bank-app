05d6c5c0e6ed90ebf5aaa086a94e57a318eed5b6
Savo Kos <savo.kos.sk@gmail.com>
2021-08-31 20:37:54 +0200

    Removes unnecessary image domains

--- next.config.js
+++ next.config.js
@@ -5,11 +5,9 @@ module.exports = {
       'http://localhost:3000',
       'https://savokos.com',
       'https://excellence.savokos.com',
-      'excellence.savokos.com',
       'https://firebasestorage.googleapis.com',
       'https://firebasestorage.googleapis.com/v0/b/excellence-holdings.appspot.com/o/images',
       'https://excellence.savokos.com/nextimg',
-      'firebasestorage.googleapis.com',
     ],
   },
 };
